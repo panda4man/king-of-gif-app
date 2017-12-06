3e9c6c74572dcc597e000521af6e195ccb3451e4
Andrew Clinton <ddrew.clinton@gmail.com>
2017-12-06 16:41:09 -0500

    added new search method

--- resources/assets/js/services/giphy-service.js
+++ resources/assets/js/services/giphy-service.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 
 export default class GiphyService {
-    static random(tag = '', rating = 'g') {
+    /**
+     * Fetch a random gif from our api.
+     *
+     * @param tag
+     * @param rating
+     */
+    static random(tag = '', rating = 'pg') {
         let url = '/giphy/random';
 
         return Vue.prototype.$http.get(url, {
@@ -9,6 +15,28 @@ export default class GiphyService {
                 tag,
                 rating
             }
+        });
+    }
+
+    /**
+     * Run a search against the giphy api.
+     *
+     * @param query
+     * @param rating
+     * @param limit
+     * @param offset
+     */
+    static search(query = '', rating = 'pg', limit = null,  offset = null) {
+        let url = '/giphy/search';
+        let params = {
+            query,
+            rating,
+            limit,
+            offset
+        };
+
+        return Vue.prototype.$http.get(url, {
+            params: params
         });
     }
 }

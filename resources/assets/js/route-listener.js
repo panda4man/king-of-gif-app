1743d7775311d9b8e629c523e71ae17aba338c9d
Andrew Clinton <ddrew.clinton@gmail.com>
2017-12-06 16:40:53 -0500

    changed lobby to room

--- resources/assets/js/route-listener.js
+++ resources/assets/js/route-listener.js
@@ -3,9 +3,9 @@ import SocketManager from './socket-manager'
 
 Router.beforeEach((to, from, next) => {
     let manager = new SocketManager;
-    console.log(to, from);
 
-    if(to.name === 'landing' && from.name === 'lobby') {
+    //Going from lobby to landing means you get booted :)
+    if(to.name === 'landing' && from.name === 'room') {
         manager.socket.emit('room-left', from.params.roomCode);
     }
 

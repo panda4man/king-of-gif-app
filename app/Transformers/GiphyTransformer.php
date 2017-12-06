2439ac53b81fc060899ad7d1b182c98fc1fff411
Andrew Clinton <ddrew.clinton@gmail.com>
2017-12-06 16:37:44 -0500

    made the transformer a little more flexible

--- app/Transformers/GiphyTransformer.php
+++ app/Transformers/GiphyTransformer.php
@@ -14,12 +14,31 @@ class GiphyTransformer extends TransformerAbstract
      */
     public function transform($data = [])
     {
-        return [
-            'id'      => $data['id'],
-            'url'     => $data['image_url'],
-            'width'   => $data['image_width'],
-            'height'  => $data['image_height'],
-            'caption' => $data['caption'],
+        $resData = [
+            'id' => $data['id'],
         ];
+
+        if(isset($data['images'])) {
+            //This will be in the case many gifs are returned
+            $original = $data['images']['original'];
+            $resData['url'] = $original['url'];
+            $resData['width'] = $original['width'];
+            $resData['height'] = $original['height'];
+
+            if(isset($data['images']['caption'])) {
+                $resData['caption'] = $data['images']['caption'];
+            }
+        } else {
+            //This will usually be a single gif look up response
+            $resData['url'] = $data['image_url'];
+            $resData['width'] = $data['image_width'];
+            $resData['height'] = $data['image_height'];
+
+            if(isset($data['title'])) {
+                $resData['caption'] = $data['title'];
+            }
+        }
+
+        return $resData;
     }
 }

03a30744c6ff4db16f4dcc260043c6383f3c928b
Andrew Clinton <ddrew.clinton@gmail.com>
2017-12-09 02:26:42 -0500

    update game component

--- resources/assets/js/components/Game.vue
+++ resources/assets/js/components/Game.vue
@@ -1,14 +1,29 @@
 <template>
-
+    <div>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script type="text/babel">
+    import SocketManager from '../socket-manager'
+    import swal from 'sweetalert2'
+
     export default {
         name: 'game',
+        socketManager: null,
         data() {
             return {
 
             }
+        },
+        created() {
+            this.socketManager = new SocketManager();
+
+            /* The host left the game */
+            this.socketManager.socket.on('host-left', () => {
+                swal('Host Left', 'Looks like the host abandoned you...', 'error');
+                this.$router.replace({name: 'landing'});
+            });
         }
     }
 </script>

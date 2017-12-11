83e4f8aacc1e2898a42fa0c431bb37c83d8c4ca6
Andrew Clinton <ddrew.clinton@gmail.com>
2017-12-10 21:34:41 -0500

    added some global game socket event handlers

--- resources/assets/js/components/Game.vue
+++ resources/assets/js/components/Game.vue
@@ -13,16 +13,44 @@
         socketManager: null,
         data() {
             return {
-
+                gameId: null,
+                joining: true,
+                popup: null
             }
         },
         created() {
             this.socketManager = new SocketManager();
+            this.gameId = this.$route.params.gameId;
 
             /* The host left the game */
             this.socketManager.socket.on('host-left', () => {
                 swal('Host Left', 'Looks like the host abandoned you...', 'error');
                 this.$router.replace({name: 'landing'});
+            });
+
+            /* Tell the server you joined the game */
+            this.socketManager.socket.emit('game-joined', this.gameId);
+
+            /* Server allowed joining of the game */
+            this.socketManager.socket.on('game-joined-success', (game) => {
+                this.$router.push({name: 'roundOne', params: {gameId: game.id}});
+            });
+
+            /* Tried to join nonexistent game */
+            this.socketManager.socket.on('game-joined-404', () => {
+                swal('Uh oh...', 'Could not find the game you were trying to join.', 'error');
+                this.$router.replace('landing');
+            });
+
+            /* Some error happened when joining the game */
+            this.socketManager.socket.on('game-joined-400', (res) => {
+                if(res.constructor === Object && res['error']) {
+                    swal('Uh oh', res.error, 'error');
+                } else {
+                    swal('Uh oh', 'There was an error joining the game. Sorry!', 'error');
+                }
+
+                this.$router.replace('landing');
             });
         }
     }
